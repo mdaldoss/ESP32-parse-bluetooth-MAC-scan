165ac231901b8b4d94874699fd359e9595ca062d
marcoDaldoss <marcodaldoss7@gmail.com>
2021-08-27 12:37:47 +0200

    Update messages

--- ESP32-parse-bluetooth-MAC-scan.ino
+++ ESP32-parse-bluetooth-MAC-scan.ino
@@ -1,21 +1,13 @@
 /***************************************************
- Copyright (c) 2021 Marco Daldoss
-  > This sofware shows how to extract and filter a MAC adress based on string match. 
+Copyright (c) 2021 Marco Daldoss
+> This sofware shows how to extract and filter a MAC adress based on string match. 
   The list of mac adress can be provided e.g. from a scan of bluetooth classic coming ESP32
 
-   Date: August 2021 
-
-  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License
-
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License
+August 2021
 ****************************************************/
- 
- 
-
-
-
 #include <string>         // std::string
-
 
 // Function that print mac adress os serial 
 void printMac(uint8_t * mac){
@@ -30,7 +22,6 @@ void printMac(uint8_t * mac){
 }
 
 int lookForMAC(uint8_t macAdress[], String input_str, String strDelimiter="Address: "){
-
     // Start looking for strDelimiter pattern match from the end of the sting in order to avoid to be 
     // mislead from Bluetooth names similar to patterns
     int found = input_str.lastIndexOf(strDelimiter);
@@ -64,20 +55,10 @@ void setup() {
     else{
       Serial.println("Mac adress not found!");
     }
-  
-  /* E.g. The outcome becomes on the serial monitor in this case would be:
-   
-   "BT mac adress Parser - 2021 Marco Daldoss
-   Parsing Bluetooth mac adress: 
-   Mac adress found:
-   0x70 0xCF 0x49 0x30 0xE0 0x66"
-
- */
 
 }
 
  
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # ESP32-parse-bluetooth-MAC-scan
 This repo shows how to parse a string in Arduino e.g. the mac address at the output of a bluetooth classic scan on a ESP32
 
-# Info
+## Info
 This sofware shows how to extract and filter a MAC adress based on string match. 
 The list of mac adress can be provided e.g. from a scan of bluetooth classic coming ESP32
 
-   
-# Author:
+
+## Example
+
+From the following input string: 
+>    "Found a device asynchronously: Name: CPDANDREA, Address: 70:cf:49:30:e0:66, cod: 27527, rssi: -59"
+    
+It will extract the following mac adress splitted in 6 element byte array:
+>   0x70 0xCF 0x49 0x30 0xE0 0x66"
+
+## Author:
  [Marco Daldoss](https://www.linkedin.com/in/marcodaldoss/)
